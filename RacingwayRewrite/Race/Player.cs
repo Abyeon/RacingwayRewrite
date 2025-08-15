1a0bef28343c1824fa67c8857baea6d52b1a4b67
Abyeon <wpseals@gmail.com>
2025-08-14 23:36:14 -0400

    Basic player movement tracking

---
+++ RacingwayRewrite/Race/Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Numerics;
+using Dalamud.Game.ClientState.Objects.SubKinds;
+using Dalamud.Game.ClientState.Objects.Types;
+
+namespace RacingwayRewrite.Race;
+
+public class Player
+{
+    public uint Id { get; set; }
+    public string Name { get; set; }
+    public uint HomeworldRow { get; set; }
+    public Vector3 Position { get; set; }
+
+    public Player(IBattleChara actor)
+    {
+        IPlayerCharacter? character = actor as IPlayerCharacter;
+        if (character != null)
+        {
+            this.Id = character.EntityId;
+            this.Name = character.Name.ToString();
+            this.HomeworldRow = character.HomeWorld.RowId;
+            this.Position = character.Position;
+            Plugin.Log.Debug($"Player {this.Name} added");
+        }
+        else
+        {
+            Plugin.Log.Error($"Unable to add player {actor.EntityId}");
+            throw new NullReferenceException("Player character was null");
+        }
+    }
+}
---
+++ RacingwayRewrite/Race/RaceManager.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using Dalamud.Game.ClientState.Objects.Types;
+using Dalamud.Plugin.Services;
+
+namespace RacingwayRewrite.Race;
+
+public class RaceManager
+{
+    internal readonly Plugin Plugin;
+    internal readonly IFramework Framework;
+    internal readonly IObjectTable ObjectTable;
+    internal readonly IClientState ClientState;
+    
+    public RaceManager(Plugin plugin, IFramework framework, IObjectTable objectTable, IClientState clientState)
+    {
+        this.Plugin = plugin;
+        this.Framework = framework;
+        this.ObjectTable = objectTable;
+        this.ClientState = clientState;
+        
+        Framework.Update += Update;
+    }
+
+    private IBattleChara? localPlayer = null;
+    
+    private void Update(IFramework framework)
+    {
+        localPlayer = ClientState.LocalPlayer;
+        if (localPlayer == null) return; // Return if the player is null
+        if (ClientState.IsPvPExcludingDen) return; // Return if the player is in pvp
+
+        TrackPlayer(localPlayer);
+
+        if (Plugin.Configuration.TrackOthers)
+        {
+            IEnumerable<IBattleChara> playersToTrack = ObjectTable.PlayerObjects.Where(x => x.EntityId != localPlayer.EntityId);
+            List<uint> touchedIds = new List<uint>();
+            
+            foreach (var player in ObjectTable.PlayerObjects)
+            {
+                TrackPlayer(player);
+                touchedIds.Add(player.EntityId);
+            }
+            
+            // Cleanup old/lost players
+            if (playersToTrack.Count() + 1 != players.Count)
+            {
+                List<uint> keysToRemove = players.Keys.Where(key => !touchedIds.Contains(key)).ToList();
+                foreach (var key in keysToRemove)
+                {
+                    players.Remove(key);
+                }
+            }
+        } else if (players.Count > 1)
+        {
+            // Remove tracked players after client disabled "track others"
+            foreach (var player in players.Keys.Where(key => key != localPlayer.EntityId))
+            {
+                players.Remove(player);
+            }
+        }
+    }
+
+    private Dictionary<uint, Player> players = new Dictionary<uint, Player>();
+    
+    private void TrackPlayer(IBattleChara actor)
+    {
+        if (!players.ContainsKey(actor.EntityId))
+        {
+            Player player = new Player(actor);
+            players.Add(actor.EntityId, player);
+            PlayerMoved(player, actor.Position);
+        }
+        else
+        {
+            Player player = players[actor.EntityId];
+            if (player.Position != actor.Position)
+            {
+                // Player moved
+                PlayerMoved(player, actor.Position);
+            }
+        }
+    }
+
+    private void PlayerMoved(Player player, Vector3 position)
+    {
+        Vector3 lastPos = player.Position;
+        player.Position = position;
+        //Plugin.Log.Debug($"{player.Name} moved from {lastPos} to {position}");
+    }
+}
